4fe3e6836878e2bc6b73b4db98d913009da4a0b2
RafaelMS <martinsoeder35@gmail.com>
2025-04-23 13:43:28 +0200

    manejados errores, también falta comprobar leaks

--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -6,18 +6,24 @@
 /*   By: rafael-m <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 19:01:06 by rafael-m          #+#    #+#             */
-/*   Updated: 2025/04/23 12:54:58 by rafael-m         ###   ########.fr       */
+/*   Updated: 2025/04/23 13:37:49 by rafael-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "libft.h"
+#include <limits.h>
 
 void	ft_putptr(void *ptr)
 {
 	char    *hex;
 	unsigned long	nbr;
 
+	if (!ptr)
+	{
+		write(1, "(nil)", 6);
+		return ;
+	}
 	nbr = (unsigned long)ptr;
 	hex = "0123456789abcdef";
 	write(1, "0x", 2);
@@ -26,13 +32,12 @@ void	ft_putptr(void *ptr)
 
 void	ft_putuint(unsigned int n)
 {
-	int	nbr;
+	char	c;
 
-	nbr = n;
-	if (nbr > 9)
+	if (n > 9)
 		ft_putuint(n / 10);
-	nbr = (n % 10) + 48;
-	write (1, &nbr, 1);
+	c = (n % 10) + 48;
+	write (1, &c, 1);
 }
 
 void	ft_puthex_lower(unsigned int n)
--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 
 int     main(void)
 {
-        char    c = 'a';
-	unsigned int	u = 123;
-	char	*str = "HOLA";
+        char    c = 1241233;
+	char	*u = 123121;
+	char	str[5] = {'H', -12123123123, 98, '\0', 'c'};
 
-	ft_printf("prueba %%c = %c; prueba %%d = %d; prueba %%p = %p, prueba %%s = %s, prueba %%x =  %x, prueba %%X =  %X, prueba %%u = %u", c, c, &c, str, u, u, u);
-	printf("prueba %%c = %c; prueba %%d = %d; prueba %%p = %p, prueba %%s = %s, prueba %%x =  %x, prueba %%X =  %X, prueba %%u = %u", c, c, &c, str, u, u, u);
+	ft_printf("prueba %%c = %c; prueba %%d = %d; prueba %%p = %p, prueba %%s = %s, prueba %%x =  %x, prueba %%X =  %X, prueba %%u = %u", c, c, u, str, u, u, u);
+	printf("prueba %%c = %c; prueba %%d = %d; prueba %%p = %p, prueba %%s = %s, prueba %%x =  %x, prueba %%X =  %X, prueba %%u = %u", c, c, u, str, u, u, u);
   	write(1, "\n", 1);
 }
